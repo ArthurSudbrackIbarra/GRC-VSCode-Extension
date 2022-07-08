808f745b4b9fc61d43ae4b2b949f9d64043ec2cf
MagicManatee <arthur.ibarra@edu.pucrs.br>
2022-07-08 16:59:09 -0300

    Installation for MacOS.

--- src/grc/installation.ts
+++ src/grc/installation.ts
@@ -13,9 +13,11 @@ export enum GRCInstallationStatus {
 }
 
 const GRC_DOWNLOAD_URL_WINDOWS =
-  "https://raw.githubusercontent.com/ArthurSudbrackIbarra/GitHub-Repo-Creator/improve-unix-installation-process/grc-install.ps1";
-const GRC_DOWNLOAD_URL_LINUX_MACOS =
-  "https://raw.githubusercontent.com/ArthurSudbrackIbarra/GitHub-Repo-Creator/improve-unix-installation-process/grc-install.sh";
+  "https://raw.githubusercontent.com/ArthurSudbrackIbarra/GitHub-Repo-Creator/improve-unix-installation-process/grc-install-windows.ps1";
+const GRC_DOWNLOAD_URL_LINUX =
+  "https://raw.githubusercontent.com/ArthurSudbrackIbarra/GitHub-Repo-Creator/improve-unix-installation-process/grc-install-linux.sh";
+const GRC_DOWNLOAD_URL_MACOS =
+  "https://raw.githubusercontent.com/ArthurSudbrackIbarra/GitHub-Repo-Creator/improve-unix-installation-process/grc-install-macos.sh";
 
 export function installGRC(
   targetDirectory: string | null
@@ -44,12 +46,17 @@ export function installGRC(
       const terminal = vscode.window.createTerminal({
         name: "GRC Installer",
         shellPath: "/bin/bash",
+        cwd: "/home",
       });
-      terminal.sendText(
-        `sudo -- sh -c 'wget ${GRC_DOWNLOAD_URL_LINUX_MACOS} && bash ./grc-install.sh && rm -f grc-install.sh'`
-      );
+      const command =
+        process.platform === "linux"
+          ? `sudo -- sh -c 'wget ${GRC_DOWNLOAD_URL_LINUX} && bash grc-install-linux.sh && rm -f grc-install-linux.sh'`
+          : `sudo -- sh -c 'curl ${GRC_DOWNLOAD_URL_MACOS} -o grc-installer.sh && bash grc-installer.sh && rm -f grc-installer.sh'`;
+      terminal.sendText(command);
       terminal.show();
-      vscode.window.showInformationMessage("Please enter your sudo password to install GitHub Repository Creator (GRC).");
+      vscode.window.showInformationMessage(
+        "Please enter your sudo password to install GitHub Repository Creator (GRC)."
+      );
       return GRCInstallationStatus.inProgress;
     } catch (error) {
       console.error(error);
